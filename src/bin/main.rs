c9caad9f1200252dcb1ca85dc96d5f8751bef9e1
CoderfishGzh <335247945@qq.com>
2022-10-14 15:28:53 +0800

    完成多线程web-server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,22 @@
 use std::fs;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
+    // 创建有4个线程的线程池子
+    let pool = ThreadPool::new(4);
+
+    // 读取信息流接受端
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        // 每读取到一个请求，就从线程池里选择一个空闲的线程执行 handle_connection 这个闭包函数
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-use std::sync::mpsc;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
 
 pub struct ThreadPool {
@@ -9,50 +8,64 @@ pub struct ThreadPool {
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
-/// 创建线程池
-///
-/// 线程池中线程的数量
-///
-/// # Panics
-///
-/// ‘new’ 函数在size为0时会panic
 impl ThreadPool {
+    /// 创建线程池
+    ///
+    /// 线程池中的线程数量
+    ///
+    /// # Panics
+    /// 'new' 函数在 size 为 0 时会 panic
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        // 创建一个信道
         let (sender, receiver) = mpsc::channel();
 
         let receiver = Arc::new(Mutex::new(receiver));
 
+        // 创建 size 个 线程
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            // create some threads and store them in the vector
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
+
         ThreadPool { workers, sender }
     }
 
+    /// 接受一个闭包函数
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        // 创建一个 job
         let job = Box::new(f);
+        // 将该job 推到信道里面
         self.sender.send(job).unwrap();
     }
 }
 
 struct Worker {
+    // 线程的 id 号
     id: usize,
-    thread: thread::JoinHandle<()>,
+    // 线程的句柄 可以多线程共享的加锁的信道接受端
+    thread: thread::JoinHandle<Arc<Mutex<mpsc::Receiver<Job>>>>,
 }
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+        // 工人新建以后会在自己的线程里，持续去获取信道的job
+        let thread = thread::spawn(move || loop {
+            // 获取receiver的锁，读取一个Job内容
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {} got a job; executing.", id);
+
+            // Job 是一个 闭包函数，可以直接 () 使用
+            job();
         });
 
         Worker { id, thread }
     }
 }
+
